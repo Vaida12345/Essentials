35bd9fe0dd24fa4e0cec6fe7edd82edc32b03847
Vaida <developer@vaidaf.net>
2024-12-01 10:39:26 +1100

    queue documentations

--- Sources/Essentials/Data Structures/Queue.swift
+++ Sources/Essentials/Data Structures/Queue.swift
@@ -1,6 +1,6 @@
 //
 //  Queue.swift
-//  The Stratum Module - Algorithms
+//  Algorithms
 //
 //  Created by Vaida on 10/17/22.
 //  Copyright © 2019 - 2024 Vaida. All rights reserved.
@@ -8,18 +8,22 @@
 
 
 /// First in, first out Queue.
-public struct Queue<Element>: CustomStringConvertible {
+///
+/// A queue operates considerably faster than an `Array` when both ``enqueue(_:)`` and ``dequeue()`` operations are required. If only `enqueue` is needed, using `Array.append` would outperform `enqueue` because `enqueue` involves individually allocating each node.
+public struct Queue<Element> {
     
     private var first: Node?
     
     private var last: Node?
     
     /// The number of elements in the queue.
+    ///
+    /// - Complexity: O(*0*), stored property.
     public private(set) var count: Int
     
     /// An one-directional Node
     private final class Node {
-        var content: Element
+        let content: Element
         var next: Node?
         
         init(_ content: Element) {
@@ -29,22 +33,10 @@ public struct Queue<Element>: CustomStringConvertible {
     }
     
     /// Returns whether the queue is empty.
+    ///
+    /// - Complexity: O(*1*)
     public var isEmpty: Bool {
         first == nil && last == nil
-    }
-    
-    /// The description to the queue.
-    public var description: String {
-        var current = self.first
-        var elements: [String] = []
-        elements.reserveCapacity(self.count)
-        
-        while let _current = current {
-            elements.append("\(_current.content)")
-            current = _current.next
-        }
-        
-        return "[" + elements.joined(separator: ", ") + "]"
     }
     
     /// Crates an empty queue.
@@ -55,6 +47,8 @@ public struct Queue<Element>: CustomStringConvertible {
     }
     
     /// Append an element to the last.
+    ///
+    /// - Complexity: O(*1*)
     public mutating func enqueue(_ element: Element) {
         let node = Node(element)
         
@@ -70,15 +64,15 @@ public struct Queue<Element>: CustomStringConvertible {
             self.last = node
         }
         
-        self.count += 1
+        self.count &+= 1
     }
     
     /// Removes and returns the first element in the queue.
+    ///
+    /// - Complexity: O(*1*)
     public mutating func dequeue() -> Element? {
-        guard first != nil else { return nil }
-        assert(last != nil)
+        guard let first = self.first else { return nil }
         
-        let first = self.first!
         if self.last === first {
             self.first = nil
             self.last = nil
@@ -86,7 +80,7 @@ public struct Queue<Element>: CustomStringConvertible {
             self.first = self.first!.next
         }
         
-        count -= 1
+        count &-= 1
         return first.content
     }
     
@@ -96,6 +90,8 @@ public struct Queue<Element>: CustomStringConvertible {
 extension Queue: IteratorProtocol {
     
     /// Returns the next element in the queue.
+    ///
+    /// - Complexity: O(*1*), alias to ``dequeue()``.
     @inlinable
     public mutating func next() -> Element? {
         self.dequeue()
@@ -104,3 +100,33 @@ extension Queue: IteratorProtocol {
 }
 
 extension Queue: Sequence { }
+
+extension Queue: CustomStringConvertible {
+    
+    /// The description to the queue.
+    public var description: String {
+        var current = self.first
+        var elements: [String] = []
+        elements.reserveCapacity(self.count)
+        
+        while let _current = current {
+            elements.append("\(_current.content)")
+            current = _current.next
+        }
+        
+        return "[" + elements.joined(separator: ", ") + "]"
+    }
+    
+}
+
+extension Queue: ExpressibleByArrayLiteral {
+    
+    public init(arrayLiteral elements: Element...) {
+        self.init()
+        
+        for element in elements {
+            self.enqueue(element)
+        }
+    }
+    
+}
