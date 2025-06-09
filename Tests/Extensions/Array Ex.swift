744944cb82e50e5e2ad4a3366d378e9e97f4c57d
Vaida <developer@vaidaf.net>
2025-06-09 19:37:19 +1000

    implement Array.stride

--- Tests/Extensions/Array Ex.swift
+++ Tests/Extensions/Array Ex.swift
@@ -90,4 +90,10 @@ struct CollectionTests {
         #expect(Array<Int>.repeating(3, count: 2) == [3, 3])
     }
     
+    @Test
+    func stride() async throws {
+        #expect(Array.stride(from: 0, by: 1, count: 10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        #expect(Array.stride(from: 0, through: 10, count: 11) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    }
+    
 }
